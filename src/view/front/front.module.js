4a5b5e0c5e403f322d0a1d6712b57c08e02ae0d2
phusick <phusick@gmail.com>
2014-10-22 15:54:30 +0200

    Style.

--- src/view/front/front.module.js
+++ src/view/front/front.module.js
@@ -8,9 +8,13 @@ angular.module('eo.view.front', [])
     $stateProvider.state('top.front', {
       url: '/',
       templateUrl: 'src/view/front/front.html',
-      controller: ['$scope', 'timerService', 'core.i18n.translationCache', function($scope, timerService, translationCache) {
-        timerService.instrument($scope);
-        console.log(translationCache.info());
+      controller: [
+        '$scope',
+        'timerService',
+        'core.i18n.translationCache',
+        function($scope, timerService, translationCache) {
+          timerService.instrument($scope);
+          console.log(translationCache.info());
       }],
       resolve: {
         test: ['core.i18n.translationCache', function(translationCache) {
