c28cf82f20ea751d1d6a4f3e5ad70a19a01c6976
phusick <phusick@gmail.com>
2014-10-22 13:55:04 +0200

    Delete unused files.

--- src/core/router/extras/fsfactories/iframe.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var iframeStateFactory = function($q, futureState) {
-  var state = {
-    name: futureState.stateName,
-    template: "<iframe src='" + futureState.src + "'></iframe>",
-    url: futureState.pathFragment
-  };
-  return $q.when(state);
-};
