68e4c0e6186808ff4cc1ba3283354fda8bea15be
phusick <phusick@gmail.com>
2014-10-31 14:06:17 +0100

    Style.

--- src/core/i18n/templateFactoryDecorator.js
+++ src/core/i18n/templateFactoryDecorator.js
@@ -11,7 +11,7 @@ angular.module('eo.core.i18n')
       var fromUrl = $delegate.fromUrl;
       $delegate.fromUrl = function(url, params) {
         var promise = fromUrl(url, params);
-        if(null === promise) { return null };
+        if(null === promise) { return null; }
         return promise.then(function(template) {
           var i18n = templateParser(template);
           translationCache.put(url, i18n);
